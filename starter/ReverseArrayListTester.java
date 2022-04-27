75881a9435830b402125b2d97fdc38e1f05e5b5a
sanj06 <52536720+sanj06@users.noreply.github.com>
2022-04-26 20:24:20 -0700

    initial commit

---
+++ starter/ReverseArrayListTester.java
@@ -0,0 +1,63 @@
+/**
+ * Tests to check the implementation of reverseRegion method in MyArrayList.java
+ */
+
+//other import statements
+
+import org.junit.*;
+
+//IMPORTANT: DO NOT MODIFY THE TEST HEADERS!
+/**
+ * This class contains various test cases to test the reverseRegion method
+ */
+public class ReverseArrayListTester {
+
+    /**
+     * Run before every test
+     */
+    @Before
+    public void setUp(){
+
+    }
+
+
+    /**
+     * Tests reverseRegion method when
+     * fromIndex and toIndex are out of bounds
+     */
+    @Test
+    public void testReverseIndexOutOfBounds(){
+        //TODO: Add your test case
+    
+    }
+
+    /**
+     * Tests reverseRegion method when
+     * fromIndex > toIndex
+     */
+    @Test
+    public void testReverseFromIndexGreater(){
+        //TODO: Add your test case
+    
+    }
+
+    /**
+     * Tests reverseRegion method when
+     * fromIndex and toIndex are within bounds
+    */
+    @Test
+    public void testReverseIndexWithinBounds(){
+        //TODO: Add your test case
+    
+    }
+
+    /**
+     * Custom test
+    */
+    @Test
+    public void testReverseCustom(){
+        //TODO: Add your test case
+
+    }
+
+}
